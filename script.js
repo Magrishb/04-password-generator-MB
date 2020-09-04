59a6cb05c6bb09eb8d781b7123e3d47f4e38e231
Magrishb <bmagrish@gmail.com>
2020-09-03 21:49:48 -0500

    Added var to each chosentype.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//Steps:
+/**
+ * 1. Ask for password length from 12 to 128 char
+ * 2. Validate the input
+ * */
+var passwordLength = alert();
+var isLowerCase = alert();
+var isUpperCase = alert();
+var isNumber = alert();
+var isSpecialChar = alert();
 
 // Write password to the #password input
 function writePassword() {
